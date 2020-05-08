ac471a82548e38dffa6773c974d0181e4765a461
qqpwn <31859931+qqpwn@users.noreply.github.com>
2020-05-08 13:39:02 +0200

    homepage fix

--- ZealandRoomBooking/View/LokaleBookingSide.xaml.cs
+++ ZealandRoomBooking/View/LokaleBookingSide.xaml.cs
@@ -20,9 +20,9 @@ namespace ZealandRoomBooking.View
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class HomePage : Page
+    public sealed partial class LokaleBookingSide : Page
     {
-        public HomePage()
+        public LokaleBookingSide()
         {
             this.InitializeComponent();
         }
